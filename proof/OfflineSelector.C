82bb4c787e73106c80c29a10780edfd1efd2042f
Andres Vargas <vargasa@cua.edu>
2020-02-06 18:03:02 +0100

    Add OfflineSelector for testing purposes

---
+++ proof/OfflineSelector.C
@@ -0,0 +1,23 @@
+#include "TProof.h"
+#include <array>
+#include <string>
+#include <fstream>
+
+Int_t OfflineSelector(TString rootfile = "", Int_t fWorkers = 4){
+
+  TChain* fChain = new TChain("Events");
+
+  std::cout << Form("Chaining %s\n",rootfile.Data()) ;
+  fChain->AddFile(rootfile.Data());
+
+  TProof *fProof = TProof::Open(Form("workers=%d",fWorkers));
+  fProof->SetProgressDialog(false);
+  fProof->SetParameter("Mass",1400);
+  fProof->SetParameter("SampleName",rootfile.Data());
+
+  fChain->SetProof();
+  fChain->Process("PreSelector.C+");
+  fProof->Print("a");
+
+  return 0;
+}
