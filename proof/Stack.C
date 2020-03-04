59fef0c06823e978c121ee4a9aca7fa85d2ee72c
Andres Vargas <vargasa@cua.edu>
2020-03-04 15:35:45 -0500

    Ntuple for graph titles

--- proof/Stack.C
+++ proof/Stack.C
@@ -16,11 +16,11 @@ void Stack(){
 
   auto f1 = TFile::Open(FileName.c_str());
 
-  std::vector<std::string> HistNames = {
-    "HMetA",
-    "HMetB",
-    "HMetC",
-    "HMetD"
+  std::vector<std::tuple<std::string,std::string>> HistNames = {
+    std::make_tuple("HMetA","3e0#mu;#slash{M}^{3e0#mu}_{T};Event count"),
+    std::make_tuple("HMetB","2e1#mu;#slash{M}^{2e1#mu}_{T};Event count"),
+    std::make_tuple("HMetC","1e2#mu;#slash{M}^{1e2#mu}_{T};Event count"),
+    std::make_tuple("HMetD","0e3#mu;#slash{M}^{0e3#mu}_{T};Event count")
   };
 
   auto c1 = new TCanvas("cs","cs",10,10,700,900);
@@ -30,17 +30,17 @@ void Stack(){
   for (auto HN : HistNames){
     THStack *hs = new THStack("hs","");
     for (auto BGN: BgNames) {
-      auto h = (TH1F*)f1->Get(Form("%s/%s",(std::get<1>(BGN)).c_str(),HN.c_str()));
+      auto h = (TH1F*)f1->Get(Form("%s/%s",(std::get<1>(BGN)).c_str(),(std::get<0>(HN).c_str())));
       //h->Scale(std::get<3>(BGN));
       h->SetFillStyle(1001);
-      h->SetTitle((std::get<1>(BGN)).c_str());
+      h->SetTitle((std::get<0>(BGN)).c_str());
       h->SetFillColor(std::get<2>(BGN));
       hs->Add(h);
     }
-    hs->SetTitle(HN.c_str());
+    hs->SetTitle((std::get<1>(HN)).c_str());
     hs->Draw();
     gPad->BuildLegend(0.5,0.5,0.95,0.95,"");
-    c1->Print(Form("Stack_%s.png",HN.c_str()));
+    c1->Print(Form("Stack_%s.png",(std::get<0>(HN)).c_str()));
     delete hs;
   }
 }
