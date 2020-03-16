18f8e5a78599275572a8f3f80de3e93a11ead5ca
Andres Vargas <vargasa@cua.edu>
2020-03-16 15:52:01 -0400

    Add Signal to Stack plot

--- proof/Stack.C
+++ proof/Stack.C
@@ -3,15 +3,17 @@
 void Stack(){
 
   std::string FileName = "WprimeHistos_all.root";
-  // ShortName, DasName, kColor, XSection
-  std::vector<std::tuple<std::string,std::string,Int_t,Float_t>> BgNames = {
-    std::make_tuple("DYJetsToLL","DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8",kYellow,1.),
-    std::make_tuple("TTJets_DiLept","TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",kGreen,2.412e02),
-    std::make_tuple("ZGToLLG","ZGToLLG_01J_LoosePtlPtg_5f_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8",kOrange,7.564e01),
-    std::make_tuple("DYJetsToLL_Zpt-100to200","DYJetsToLL_Zpt-100to200_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",kRed,4.102),
-    std::make_tuple("WZTo3LNu","WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8",kMagenta,4.102),
-    std::make_tuple("ZZTo4L","ZZTo4L_13TeV_powheg_pythia8",kBlue,1.256),
-    std::make_tuple("DYJetsToLL","DYJetsToLL_Zpt-200toInf_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",kCyan,1.030e+02)
+  const Int_t FillStyle = 1001;
+  // ShortName, DasName, kColor, Style, XSection
+  std::vector<std::tuple<std::string,std::string,Int_t,Int_t,Float_t>> BgNames = {
+    std::make_tuple("DYJetsToLL","DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8",kYellow,FillStyle,1.),
+    std::make_tuple("TTJets_DiLept","TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",kGreen,FillStyle,2.412e02),
+    std::make_tuple("ZGToLLG","ZGToLLG_01J_LoosePtlPtg_5f_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8",kOrange,FillStyle,7.564e01),
+    std::make_tuple("DYJetsToLL_Zpt-100to200","DYJetsToLL_Zpt-100to200_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",kRed,FillStyle,4.102),
+    std::make_tuple("WZTo3LNu","WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8",kMagenta,FillStyle,4.102),
+    std::make_tuple("ZZTo4L","ZZTo4L_13TeV_powheg_pythia8",kBlue,FillStyle,1.256),
+    std::make_tuple("DYJetsToLL","DYJetsToLL_Zpt-200toInf_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",kCyan,FillStyle,1.030e+02),
+    std::make_tuple("Wprime 1.4TeV","WprimeToWZToWlepZlep_narrow_M-1400_13TeV-madgraph",kBlack,FillStyle,1.)
   };
 
   auto f1 = TFile::Open(FileName.c_str());
@@ -31,10 +33,10 @@ void Stack(){
     THStack *hs = new THStack("hs","");
     for (auto BGN: BgNames) {
       auto h = (TH1F*)f1->Get(Form("%s/%s",(std::get<1>(BGN)).c_str(),(std::get<0>(HN).c_str())));
-      //h->Scale(std::get<3>(BGN));
-      h->SetFillStyle(1001);
+      h->SetFillStyle(std::get<3>(BGN));
       h->SetTitle((std::get<0>(BGN)).c_str());
       h->SetFillColor(std::get<2>(BGN));
+      h->Scale(std::get<4>(BGN));
       hs->Add(h);
     }
     hs->SetTitle((std::get<1>(HN)).c_str());
