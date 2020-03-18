bf456a1c3d339eb42fcb8de88d64714a07044744
Andres Vargas <vargasa@cua.edu>
2020-03-18 11:33:39 -0400

    TeX: Add xsec computed from GenXSecAnalyzer, Add signal to stacked plot and computed signal xsec (GenXSecAnalyzer)

--- proof/Stack.C
+++ proof/Stack.C
@@ -4,25 +4,24 @@ void Stack(){
 
   std::string FileName = "WprimeHistos_all.root";
   const Int_t FillStyle = 1001;
-  // ShortName, DasName, kColor, Style, XSection
-  std::vector<std::tuple<std::string,std::string,Int_t,Int_t,Float_t>> BgNames = {
-    std::make_tuple("DYJetsToLL","DYJetsToLL_M-50_TuneCP5_13TeV-madgraphMLM-pythia8",kYellow,FillStyle,1.),
-    std::make_tuple("TTJets_DiLept","TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",kGreen,FillStyle,2.412e02),
-    std::make_tuple("ZGToLLG","ZGToLLG_01J_LoosePtlPtg_5f_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8",kOrange,FillStyle,7.564e01),
-    std::make_tuple("DYJetsToLL_Zpt-100to200","DYJetsToLL_Zpt-100to200_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",kRed,FillStyle,4.102),
-    std::make_tuple("WZTo3LNu","WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8",kMagenta,FillStyle,4.102),
-    std::make_tuple("ZZTo4L","ZZTo4L_13TeV_powheg_pythia8",kBlue,FillStyle,1.256),
-    std::make_tuple("DYJetsToLL","DYJetsToLL_Zpt-200toInf_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",kCyan,FillStyle,1.030e+02),
-    std::make_tuple("Wprime 1.4TeV","WprimeToWZToWlepZlep_narrow_M-1400_13TeV-madgraph",kBlack,FillStyle,1.)
+  // ShortName, DasName, kColor, Style, isSignal?, XSection
+  std::vector<std::tuple<std::string,std::string,Int_t,Int_t,Bool_t,Float_t>> BgNames = {
+    std::make_tuple("WZ","WZTo3LNu_TuneCUETP8M1_13TeV-powheg-pythia8",kOrange,FillStyle,false,4.102),
+    std::make_tuple("DYJetsToLL_Zpt-100to200","DYJetsToLL_Zpt-100to200_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",kOrange+7,FillStyle,false,5.795e02),
+    std::make_tuple("DYJetsToLL","DYJetsToLL_Zpt-200toInf_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",kOrange+7,FillStyle,false,1.030e+02),
+    std::make_tuple("t#bar{t}","TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8",kBlue-2,FillStyle,false,2.412e02),
+    std::make_tuple("Z#gamma","ZGToLLG_01J_LoosePtlPtg_5f_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8",kRed+3,FillStyle,false,7.564e01),
+    std::make_tuple("ZZ","ZZTo4L_13TeV_powheg_pythia8",kBlue,FillStyle,false,1.256),
+    std::make_tuple("W' (1.4TeV)","WprimeToWZToWlepZlep_narrow_M-1400_13TeV-madgraph",kBlack,0,true,3.196e02)
   };
 
   auto f1 = TFile::Open(FileName.c_str());
 
   std::vector<std::tuple<std::string,std::string>> HistNames = {
-    std::make_tuple("HMetA","3e0#mu;#slash{M}^{3e0#mu}_{T};Event count"),
-    std::make_tuple("HMetB","2e1#mu;#slash{M}^{2e1#mu}_{T};Event count"),
-    std::make_tuple("HMetC","1e2#mu;#slash{M}^{1e2#mu}_{T};Event count"),
-    std::make_tuple("HMetD","0e3#mu;#slash{M}^{0e3#mu}_{T};Event count")
+    std::make_tuple("HMassZA","3e0#mu;#slash{E}^{3e0#mu}_{T};Event count"),
+    std::make_tuple("HMassZB","2e1#mu;#slash{E}^{2e1#mu}_{T};Event count"),
+    std::make_tuple("HMassZC","1e2#mu;#slash{E}^{1e2#mu}_{T};Event count"),
+    std::make_tuple("HMassZD","0e3#mu;#slash{E}^{0e3#mu}_{T};Event count")
   };
 
   auto c1 = new TCanvas("cs","cs",10,10,700,900);
@@ -36,12 +35,13 @@ void Stack(){
       h->SetFillStyle(std::get<3>(BGN));
       h->SetTitle((std::get<0>(BGN)).c_str());
       h->SetFillColor(std::get<2>(BGN));
-      h->Scale(std::get<4>(BGN));
+      h->Scale(std::get<5>(BGN));
+      if(!std::get<4>(BGN)) h->SetLineWidth(0);
       hs->Add(h);
     }
     hs->SetTitle((std::get<1>(HN)).c_str());
-    hs->Draw();
-    gPad->BuildLegend(0.5,0.5,0.95,0.95,"");
+    hs->Draw("HIST");
+    gPad->BuildLegend(0.6,0.6,0.90,0.90,"");
     c1->Print(Form("Stack_%s.png",(std::get<0>(HN)).c_str()));
     delete hs;
   }
