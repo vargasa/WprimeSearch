804f6f70e1045c02004ff8e1797edcaafe42331c
Andres Vargas <vargasa@cua.edu>
2020-04-16 16:23:59 -0500

    Option to read EntryList file

--- proof/Selector.C
+++ proof/Selector.C
@@ -3,7 +3,7 @@
 #include <string>
 #include <fstream>
 
-Int_t Selector(std::string files = "", Int_t fWorkers = 4){
+Int_t Selector(std::string files = "", Int_t fWorkers = 4, std::string elistfile = ""){
 
   TChain* fChain = new TChain("Events");
 
@@ -21,6 +21,16 @@ Int_t Selector(std::string files = "", Int_t fWorkers = 4){
     }
     sample += file.substr(file.rfind("/")+1);
     sample.resize(sample.size()-4);
+  }
+
+  TEntryList *EList;
+  TFile *FileEList;
+
+  if(!elistfile.empty()){
+    FileEList = TFile::Open(elistfile.c_str(),"READ");
+    EList = (TEntryList*)FileEList->Get(Form("%s/EntryList",sample.c_str()));
+    std::cout << Form("Info: Using EntryList from %s\n",elistfile.c_str());
+    fChain->SetEntryList(EList);
   }
 
   TProof *fProof = TProof::Open(Form("workers=%d",fWorkers));
