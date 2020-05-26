8cebed05387641aa1eb7d92ecf8688f6110e5605
Andres Vargas <vargasa@cua.edu>
2020-05-26 12:10:34 -0400

    Include Isolated muons HLTs

--- proof/EntryListMaker/EventSelection.h
+++ proof/EntryListMaker/EventSelection.h
@@ -15,6 +15,8 @@ class EventSelection {
   TTreeReaderValue<Bool_t> HLT_Photon175 = {fReader, "HLT_Photon175"};
   TTreeReaderValue<Bool_t> HLT_Mu50 = {fReader, "HLT_Mu50"};
   TTreeReaderValue<Bool_t> HLT_TkMu50 = {fReader, "HLT_TkMu50"};
+  TTreeReaderValue<Bool_t> HLT_IsoMu24 = {fReader, "HLT_IsoMu24"};
+  TTreeReaderValue<Bool_t> HLT_IsoTkMu24 = {fReader, "HLT_IsoTkMu24"};
   TTreeReaderValue<Bool_t> Flag_globalTightHalo2016Filter = {fReader, "Flag_globalTightHalo2016Filter"};
   TTreeReaderValue<Bool_t> Flag_hcalLaserEventFilter = {fReader, "Flag_hcalLaserEventFilter"};
   TTreeReaderValue<Bool_t> Flag_EcalDeadCellTriggerPrimitiveFilter = {fReader, "Flag_EcalDeadCellTriggerPrimitiveFilter"};
@@ -47,7 +49,7 @@ void EventSelection::ReadEntry(Long64_t entry, UInt_t year){
 
   if(year == 2016){
     ElectronHLTs = (*HLT_Ele27_WPLoose_Gsf||*HLT_Ele115_CaloIdVT_GsfTrkIdT||*HLT_Photon175);
-    MuonHLTs = (*HLT_Mu50||*HLT_TkMu50);
+    MuonHLTs = (*HLT_IsoMu24||*HLT_IsoTkMu24||*HLT_Mu50||*HLT_TkMu50);
     Flags = *Flag_HBHENoiseIsoFilter && *Flag_EcalDeadCellTriggerPrimitiveFilter &&
       *Flag_globalTightHalo2016Filter && *Flag_BadPFMuonSummer16Filter
       && *PV_npvsGood > 0;
