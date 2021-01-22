565a0a5cb7b1c8ef61d18ad059e544accd10be17
Andres Vargas <vargasa@cua.edu>
2021-01-22 10:09:14 -0600

    Add ST Samples.
    xsec source: AN2019_086_v1.pdf

--- proof/Stack.C
+++ proof/Stack.C
@@ -84,6 +84,16 @@ void Stack(std::string FileName = "WprimeHistos_all.root"){
             kGreen+3,0.003339},
           BackgroundInfo{"gg","GluGluToContinToZZTo2e2mu_13TeV_MCFM701_pythia8",
             kGreen+3,0.006699},
+          BackgroundInfo{"ST","ST_s-channel_4f_leptonDecays_13TeV-amcatnlo-pythia8_TuneCUETP8M1",
+            kGreen+3,3.681},
+          BackgroundInfo{"ST","ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1",
+            kGreen+3,35.85},
+          BackgroundInfo{"ST","ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1",
+            kGreen+3,35.85},
+          BackgroundInfo{"ST","ST_t-channel_antitop_4f_inclusiveDecays_13TeV-powhegV2-madspin-pythia8_TuneCUETP8M1",
+            kGreen+3,80.95},
+          BackgroundInfo{"ST","ST_t-channel_top_4f_inclusiveDecays_13TeV-powhegV2-madspin-pythia8_TuneCUETP8M1",
+            kGreen+3,136.02},
         }
       },
       {
