9a84ef6095d641fe47332f69a30b819f1a7b61e9
Andres Vargas <vargasa@cua.edu>
2022-12-06 08:45:05 -0600

    Update Running Scripts

--- proof/EntryListMaker/MakeTrees.sh
+++ proof/EntryListMaker/MakeTrees.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+YEAR=$1
+SAMPLE=$2
+BRANCHNAME=$3
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 export SCRAM_ARCH=slc7_amd64_gcc900
 export NCORES=3
@@ -6,16 +9,16 @@ scram project CMSSW CMSSW_12_2_1
 cd CMSSW_12_2_1/src
 eval `scram runtime -sh`
 echo $CMSSW_BASE
-git clone https://github.com/vargasa/WprimeSearch.git Wprime_$1
-WprimeDir=$CMSSW_BASE/src/Wprime_$1/
+git clone --branch $BRANCHNAME https://github.com/vargasa/WprimeSearch.git Wprime_$YEAR
+WprimeDir=$CMSSW_BASE/src/Wprime_$YEAR/
 #sed -i 's/cmsxrootd.fnal.gov/xrootd-cms.infn.it/' $WprimeDir/proof/EntryListMaker/MakeEventIDTree.C
 #sed -i 's/cmsxrootd.fnal.gov/xrootd.unl.edu/' $WprimeDir/proof/EntryListMaker/MakeEventIDTree.C
-#sed -i 's/cmsxrootd.fnal.gov/cms-xrd-global.cern.ch/' $WprimeDir/proof/EntryListMaker/MakeEventIDTree.C
-OutputLabel="EntryListMaker_"$1"_"$2
+sed -i 's/cmsxrootd.fnal.gov/cms-xrd-global.cern.ch/' $WprimeDir/proof/EntryListMaker/MakeEventIDTree.C
+OutputLabel="EntryListMaker_"$YEAR"_"$SAMPLE
 cd $WprimeDir/proof/EntryListMaker/
-export FILES=$WprimeDir/proof/files/data/$1/UL/$2.txt
+export FILES=$WprimeDir/proof/files/data/$YEAR/UL/$SAMPLE.txt
 
-echo -e "#define Y"$1"\n#define CMSDATA\n#define ULSAMPLE">../IsData.h
+echo -e "#define Y"$YEAR"\n#define CMSDATA\n#define ULSAMPLE">../IsData.h
 root -l -b -q "MakeEventIDTree.C(\""$FILES"\","$NCORES")"
 
 cd $WprimeDir/proof/EntryListMaker/
--- proof/EntryListMaker/SubmitCondor.sh
+++ proof/EntryListMaker/SubmitCondor.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+YEAR=$1
+SAMPLE=$2
+BRANCHNAME=$3
 export NCORES=3
 export jdlString="universe = vanilla
 Executable = MakeTrees.sh
@@ -6,10 +9,10 @@ should_transfer_files = YES
 when_to_transfer_output = ON_EXIT
 request_cpus = $NCORES
 request_memory = 6000
-Output = MakeTrees_\$(Cluster)_\$(Process)_$1_$2.stdout
-Error = MakeTrees_\$(Cluster)_\$(Process)_$1_$2.stderr
-Log = MakeTrees_\$(Cluster)_\$(Process)_$1_$2.log
-Arguments = $1 $2
+Output = MakeTrees_\$(Cluster)_\$(Process)_${YEAR}_${SAMPLE}_${BRANCHNAME}.stdout
+Error = MakeTrees_\$(Cluster)_\$(Process)_${YEAR}_${SAMPLE}_${BRANCHNAME}.stderr
+Log = MakeTrees_\$(Cluster)_\$(Process)_${YEAR}_${SAMPLE}_${BRANCHNAME}.log
+Arguments = ${YEAR} ${SAMPLE} ${BRANCHNAME}
 Queue 1"
-echo "${jdlString}" > /tmp/condor_job_$1_$2.jdl
-condor_submit /tmp/condor_job_$1_$2.jdl
+echo "${jdlString}" > /tmp/condor_job_${YEAR}_${SAMPLE}_${BRANCHNAME}.jdl
+condor_submit /tmp/condor_job_${YEAR}_${SAMPLE}_${BRANCHNAME}.jdl
